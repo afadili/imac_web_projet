73632b57b237b1e774d494f1c4e0693303d8bf3d
Pierre Gabory <piergabory@gmail.com>
2018-04-19 15:34:46 +0200

    class DatabaseUpdater

--- back-end/pdo/DatabaseUpdater.class.php
+++ back-end/pdo/DatabaseUpdater.class.php
@@ -10,13 +10,37 @@ require_once "PDO/MyPDO.emoji-tracker.include.php"
 class DatabaseUpdater {
 
 	private static $activeBatch = null;
+	private static $stats = null;
 
 	// privatize constructor
 	private function __construct() {}
 
 
+	public static function newDataPoint($emoji, $hashtag, array $data) {
+		require_once "Emoji.class.php";
+		require_once "Hashtag.class.php";
+
+		// get hashtag
+		$idHashtag = Hashtag::createFromName($hashtag);
+		
+		// add hashtag to database if doesn't exist
+		if ($idHashtag === null) {
+			$idHashtag = addHashtag($hashtag);
+		}
+
+		// get emoji
+		$idEmoji = Emoji::createFromChar($emoji);
+
+		// get stat
+		$idStat = self::addStatistic($data);
+
+		self::linkStatistic($idStat, $idEmoji, $idHashtag);
+	}
+
+
+
 	// create new batch
-	private static newBatch() {
+	public static function newBatch() {
 		require_once "Batch.class.php";
 
 		$query = "
@@ -39,10 +63,65 @@ class DatabaseUpdater {
 
 	}
 
+	
+	// add line to relation table
+	private static function addRelation($idStat, $idEmoji, $idHashtag = null) {
+		$query = "
+			INSERT INTO relation(emoji,hashtag,statistics)
+			VALUES (:emoji, :hashtag, :statistics)
+		"
+
+		$stmt = MyPDO::getInstance()->prepare($query);
+		$stmt->execute(array(
+			":emoji" => $idEmoji, 
+			":hashtag" => $idHashtag, 
+			":statistics" => $idStat
+		));
+	}
+
+	// add line to the statistics table
+	private static function addStatistic($data) {
+		require_once "Statistics.class.php";
+
+		$query = "
+			INSERT INTO statistics(
+				idBatch, 
+				nbTweets, 
+				avgRetweets, 
+				avgFavorites, 
+				avgResponses, 
+				avgPopularity, 
+				bestTweet
+			) VALUES (
+				:batch,
+				:count,
+				:avgRetweets, 
+				:avgFavorites, 
+				:avgResponses, 
+				:avgPopularity, 
+				:best
+			);
+
+			SELECT id FROM batch 
+			ORDER BY date DESC 
+			LIMIT 1
+		";
+
+		$stmt = MyPDO::getInstance()->prepare($query);
+		$stmt->execute($data);
+
+		$stmt->setFetchMode(PDO::FETCH_CLASS, "Statistics");
+		if (($object = $stmt->fetch()) !== false)
+			return $object;
+		else
+			throw new Exception("Failed to create new batch");
+	}
+
+
 	/* add new hashtag
 	 * @return hashtag id
 	 */
-	private static addHashtag($word) {
+	private static function addHashtag($word) {
 		require_once "Hashtag.class.php";
 
 		$query = "
@@ -61,3 +140,4 @@ class DatabaseUpdater {
 	}
 
 }
+	
