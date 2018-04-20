40e617d30ec00cccb61a7ca2c456832ff32dc2e3
Pierre Gabory <piergabory@gmail.com>
2018-04-20 12:40:08 +0200

    changement charset pour pouvoir récuperer les emojis dans le php

--- back-end/pdo/PDO/MyPDO.emoji-tracker.include.php
+++ back-end/pdo/PDO/MyPDO.emoji-tracker.include.php
@@ -1,5 +1,5 @@
 <?php
 require_once 'MyPDO.class.php';
-
 // host=votre serveur (localhost si travail en local)
-$pdo = MyPDO::setConfiguration('mysql:localhost=mysql;dbname=emoji-tracker;charset=utf8', 'root', '');
+// note: charset utf8mb4 pour les emojis
+$pdo = MyPDO::setConfiguration('mysql:localhost=mysql;dbname=emoji-tracker;charset=utf8mb4', 'root', '');
