300d958a6db147b859d36fa1e269568842990ddd
Pierre Gabory <piergabory@gmail.com>
2018-04-20 15:11:21 +0200

    retiré test inutile

--- back-end/twitter-api/TwitterAPI.class.test.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'TwitterAPI.emoji-tracker.include.php';
-
-TwitterAPICall::connect();
-$call = new TwitterAPICall();
-echo $call->getTweetCount()."<br>";
-var_dump($call->getEmojis());
