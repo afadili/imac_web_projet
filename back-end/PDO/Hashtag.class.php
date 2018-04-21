43d77ec4ce4b0c9eb5acd8afffbfd1e917b2fd3b
Pierre Gabory <piergabory@gmail.com>
2018-04-21 18:23:55 +0200

    Hashtag documentation

--- back-end/PDO/Hashtag.class.php
+++ back-end/PDO/Hashtag.class.php
@@ -1,22 +1,36 @@
 <?php
 require_once 'PDO/MyPDO/MyPDO.emoji-tracker.include.php'; 
 
-/* -------------------------------------------------
+/**
  * HASHTAG CLASS
- *	> get id, word
- *	> get all
- *	> get from emoji
- * -------------------------------------------------
+ * Represent the 'Hashtag‘ table of the database
  */
 
 class Hashtag {
-	/* --- Attributes --- */
 
+	//// PROPERTIES
+
+	/**
+	 * @var Integer $id, id of the Hashtag
+	 */
 	private $id = null;
+
+	/**
+	 * @var String $word, text corresponding to the hashtag (character '#' excluded)
+	 */
 	private $word = null;
 
 
 
+
+	//// FACTORY
+
+	/**
+	 * CREATE FROM WORD
+	 * @param String $word, text of the hashtag
+	 * @return Hashtag instance, of the corresponding hashtag
+	 * @throws Exception if hashtag not found.
+	 */
 	public static function createFromWord($word) {
 		$stmt = MyPDO::getInstance()->prepare("SELECT * FROM hashtag WHERE word = ?");
 		$stmt->execute(array($word));
@@ -28,43 +42,76 @@ class Hashtag {
 	}
 
 
+	/**
+	 * SUDO CREATE FROM WORD
+	 * @param String $word, text of the hashtag
+	 * @return Hashtag instance, of the corresponding hashtag
+	 * If hashtag does not exists, a new row is added to the table.
+	 *
+	 * SHOULD ONLY BE CALLED BY TWITTER API SERVICE, USE createFromWord() INSTEAD.
+	 */
+	public static function sudoCreateFromWord($word) {
+		$query = "
+			INSERT INTO hashtag(word)
+			SELECT * FROM (SELECT :word) AS tmp
+			WHERE NOT EXISTS (
+			    SELECT word FROM hashtag WHERE word = :word
+			) LIMIT 1
+		";
 
-	/* --- Basic Getters --- */
-	// get id
+		$stmt = MyPDO::getInstance()->prepare($query);
+		$stmt->execute(array(":word" => "$word"));
+
+		return self::createFromWord($word);
+	}
+
+
+
+	//// BASIC GETTERS
+	
+	/**
+	 * GET ID
+	 * @return Integer $id, id of the Hashtag
+	 */
 	public function getId() { 
 		return $this->id; 
 	}
 
-	// get word
+	/**
+	 * GET WORD
+	 * @return Integer $word, text of the Hashtag
+	 */
 	public function getWord() {
 		return $this->word;
 	}
 
 
 
-	/* --- Complex Getters --- */
+	//// COMPLEX GETTERS
 
-	/* GET ALL HASHTAGS
-	 * Grabs all the moods from the database
-	 * @return array<Hashtag>
+	/** 
+	 * GET ALL HASHTAGS
+	 * Grabs all the hashtags from the database
+	 * @return array<Hashtag>, Array of instances of hashtag
 	 */
-
 	public static function getAll() {
 		$stmt = MyPDO::getInstance()->prepare("SELECT * FROM hashtag");
 		$stmt->execute();
 		$stmt->setFetchMode(PDO::FETCH_CLASS, "Hashtag");
+
 		if (($object = $stmt->fetchAll()) !== false)
 			return $object;
 		else
-			throw new Exception("Hashtag table does'nt exist? Hmmm...");
+			throw new Exception("Failed to access to Hashtag table.");
 	}
 
 
 
-	/* GET ALL HASHTAGS FROM EMOJI
-	 * return a list of instances of Hashtags used with a given emoji
-	 * @params idEmoji, integer id of the used emoji
-	 * @return array<Hashtag> list of instances of hashtags
+	/**
+	 * GET ALL HASHTAGS FROM EMOJI
+	 * returns a list of instances of Hashtags used with a given emoji
+	 * @param Integer $idEmoji, id of the used emoji
+	 * @return array<Hashtag>, array of instances of Hashtag
 	 */
 	public function getFromEmoji($idEmoji) {
 		$query = "
@@ -85,25 +132,6 @@ class Hashtag {
 
 
 
-	/* ---- CONSTRUCTOR & SETTERS ---- */
-	/* !!! WARNING: CONSTRUCTOR AND SETTERS SHOULD NEVER BE CALLED BY ANY FRONT-END REQUESTS !!! */
-
-	
-	public static function sudoCreateFromWord($word) {
-		$query = "
-			INSERT INTO hashtag(word)
-			SELECT * FROM (SELECT :word) AS tmp
-			WHERE NOT EXISTS (
-			    SELECT word FROM hashtag WHERE word = :word
-			) LIMIT 1
-		";
-
-		$stmt = MyPDO::getInstance()->prepare($query);
-		$stmt->execute(array(":word" => "$word"));
-
-		return self::createFromWord($word);
-	}
-
+	// Disable constructor
 	private function __construct() {}
-
 }
