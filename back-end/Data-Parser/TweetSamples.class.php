87c6afdae64c3017ff1d27f72e4524a5c29fac06
Pierre Gabory <piergabory@gmail.com>
2018-04-21 15:39:50 +0200

    Documentation et best tweer

--- back-end/Data-Parser/TweetSamples.class.php
+++ back-end/Data-Parser/TweetSamples.class.php
@@ -1,33 +1,69 @@
 <?php
 require_once "PDO/Statistics.class.php";
 
+/**
+ * TweetSamples Class
+ * -----------------------------------
+ * implements StatisticsData interface
+ * Set of functions to analyse a set of tweets.
+ */
+
 class TweetSamples implements StatisticsData {
 	
+	/**
+	 * @var array<Tweets>, statistic samples to analyse
+	 */
 	private $tweets;
 
-	// @param array<Tweets>
-	public function __construct($tweets) {
-		$this->tweets = $tweets;
-		
-		if (!is_array($this->tweets))
-			throw new Error("Expected parameter to be an array");
 
+	/**
+	 * Constructor
+	 * @param array<Tweets>
+	 * Array is expected to not be empty
+	 */
+	public function __construct(array $tweets) {
+		$this->tweets = $tweets;
 
 		if (self::count() <= 0)
 			throw new Error("Empty array of tweets");
 	}
 
-	// @returns integer the number of tweets retrieved
+
+	
+	/**
+	 * Count
+	 * @return Integer size of the sample of tweets
+	 */
 	public function count() {
 		return count($this->tweets);
 	}
 
-	// todo : return best tweet:
+
+	/** 
+	 * Best
+	 * @return String URL of the most popular tweet of the sample
+	 */
 	public function best() {
-		return false;
+		$best = null;
+		$max = -1;
+		foreach ($this->tweets as $tweet) {
+			if($max <= $tweet->retweet_count){
+				$best = $tweet;
+				$max = $tweet->retweet_count;
+			}
+		}
+		return "https://www.twitter.com/statuses/{$best->id}";
 	}
 
-	// @returns integer the total number of retweet through all tweets
+
+
+	/* ---- TOTALS ---- */
+
+
+	/**
+	 * Total Retweet Count
+	 * @return Integer sum of all the retweets of each tweet the sample
+	 */
 	public function totalRetweetCount() {
 
 		return array_sum(array_map( 
@@ -36,7 +72,11 @@ class TweetSamples implements StatisticsData {
 		));
 	}
 
-	// @returns integer the total number of favorites through all tweets
+
+	/**
+	 * Total Favorite Count
+	 * @return Interger sum of all the favorites of each tweet the sample
+	 */
 	public function totalFavoriteCount() {
 		return array_sum(array_map( 
 			function($tweet) {return $tweet->favorite_count;},
@@ -45,21 +85,51 @@ class TweetSamples implements StatisticsData {
 	}
 
 
-	// @return float average of retweets per tweet
+	/**
+	 * Total Response Count
+	 * @return Interger sum of all the replies of each tweet the sample
+	 */
+	public function totalResponseCount() {
+		// TODO
+		return 0; 
+	}
+
+
+
+	/* ---- AVERAGES ---- */
+
+
+	/**
+	 * Average Retweets
+	 * @return Float average of retweets per tweet in the sample
+	 */
 	public function avgRetweets() {
 		return $this->totalRetweetCount() / $this->count();
 	}
 
-	// @return float average of Favorite per tweet
+
+	/**
+	 * Average Favorite
+	 * @return Float average of favorite per tweet in the sample
+	 */
 	public function avgFavorites() {
 		return $this->totalFavoriteCount() / $this->count();
 	}
 
+
+	/**
+	 * Average Responses
+	 * @return Float average of responses per tweet in the sample
+	 */
 	public function avgResponses() {
-		return 0;
+		return $this->totalResponseCount() / $this->count();
 	}
 
+	/**
+	 * Average Popularity
+	 * @return Float average popularity of a tweet in the sample
+	 */
 	public function avgPopularity() {
-		return 0;
+		return $this->avgResponses() * $this->avgFavorites();;
 	}
 }
