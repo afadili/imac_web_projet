2d61793b9f6c5e7e9079934ec482870e27f85088
Pierre Gabory <piergabory@gmail.com>
2018-04-26 21:52:32 +0200

    Greeting page

---
+++ api/resources/views/hello.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello</title>
+</head>
+<body>
+	Hello<br>
+	C'est l'API de emoji-tracker
+	<br>
+	Si vous cherchez le site c'est ici:
+	<br>
+	<a href="http://server.piergabory.com:5000/">Emoji-tracker app</a>
+	<br>
+	TODO: présenter l'API
+</body>
+</html>
--- api/routes/web.php
+++ api/routes/web.php
@@ -12,7 +12,7 @@
 */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    return view('hello');
 });
 
 /* Chaque route aura un préfixe /api */
