110e109101d3c99d3f02947e574e06c31496511e
Pierre Gabory <piergabory@gmail.com>
2018-04-29 11:45:12 +0200

    StatisticsController documentation

--- api/app/Http/Controllers/StatisticsController.php
+++ api/app/Http/Controllers/StatisticsController.php
@@ -9,20 +9,51 @@ use App\Hashtag;
 
 use Illuminate\Http\Request;
 
-// TODO : Documentation
+/**
+ * Ranking Controller
+ * ------------------
+ * Handles and prepare responses for raw stastistics data
+ */
 class StatisticsController extends Controller
-{
+{   
+
+    /**
+     * Get Batch
+     *
+     * Get statistics from batch closest to given date
+     *
+     * @param $date
+     * @return JSON: Array<Satistics> 
+     */
     public function getBatch($date)
     {
         // TODO or DELETE
     }
 
-    // TODO: ADD MINMAX DATE
+ 
+    
+    /**
+     * Get From Emoji
+     *    
+     * Get raw statistics about an emoji
+     *
+     * @param Hexadecimal $code, Unicode
+     * @return JSON: Array<Statistics>
+     */
     public function getFromEmoji($code) {
         return response()->json(Statistics::whereEmoji($code)->get());
     }
 
-    // TODO: ADD MINMAX DATE
+    /**
+     * Get From Emoji And Hashtag
+     *    
+     * Get raw statistics about an emoji in context of an hashtag
+     *
+     * @param Hexadecimal $code, Unicode
+     * @param String $hashtag
+     *
+     * @return JSON: Array<Statistics>
+     */
     public function getFromEmojiAndHashtag($code,$hashtag) {
         return response()->json(Statistics::whereEmojiAndHashtag($code,$hashtag)->get);
     }
